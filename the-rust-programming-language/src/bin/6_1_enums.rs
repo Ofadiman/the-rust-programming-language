7b93eb3a0bd828ad43cbc24d93563eed64a192d3
ofadiman <ofadiman@gmail.com>
2024-11-07 22:44:00 +0100

    add code showcasing enum definitions

---
+++ the-rust-programming-language/src/bin/6_1_enums.rs
@@ -0,0 +1,40 @@
+use rand::Rng;
+
+fn main() {
+    results()
+}
+
+#[allow(dead_code)]
+fn extract_values_from_enum() {
+    #[derive(Debug)]
+    enum Color {
+        Rgb(u8, u8, u8),
+        Hex(String),
+    }
+
+    let number = rand::thread_rng().gen_range(1..=2);
+    let color = if number == 1 {
+        Color::Rgb(252, 186, 3)
+    } else {
+        Color::Hex("#fcba03".to_string())
+    };
+
+    let computed: String = match color {
+        Color::Rgb(red, green, blue) => format!("red: {red}, green: {green}, blue: {blue}"),
+        Color::Hex(value) => format!("hex: {value}"),
+    };
+
+    println!("{computed:#?}");
+}
+
+#[allow(dead_code)]
+fn results() {
+    let number = rand::thread_rng().gen_range(1..=2);
+    let option = if number == 1 { Some(1) } else { None };
+
+    if let Some(value) = option {
+        println!("value from enum: {value}");
+    } else {
+        println!("nothing here");
+    }
+}
