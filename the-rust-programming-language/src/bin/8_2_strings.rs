5d8285a8bea084fd18d87c1a2b5eacd1c2bc381d
ofadiman <ofadiman@gmail.com>
2024-11-09 21:15:22 +0100

    add code showcasing strings

---
+++ the-rust-programming-language/src/bin/8_2_strings.rs
@@ -0,0 +1,49 @@
+fn main() {
+    unicode();
+}
+
+#[allow(dead_code)]
+fn mutating_strings_with_plus_operator() {
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = String::from(" Yooo!");
+
+    // String concatenation requires an owned String, so `s1` had to be cloned (or moved) here.
+    let s4 = s1.clone() + &s2 + &s3;
+
+    println!("concatenated string using plus operator: {s4}")
+}
+
+#[allow(dead_code)]
+fn format_macro() {
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s4 = format!("{s1}-{s2}-{s3}");
+    println!("concatenated string using format macro: {s4}")
+}
+
+#[allow(dead_code)]
+fn indexing_strings() {
+    let hello_world = String::from("Hello, World!");
+    let hello = &hello_world[0..=4];
+    println!("hello slice: {hello:#?}");
+}
+
+#[allow(dead_code)]
+fn unicode() {
+    let greating = String::from("cześć");
+
+    // the number of bytes in "cześć" word is: 7
+    println!(
+        "the number of bytes in \"{greating}\" word is: {}",
+        greating.len()
+    );
+
+    // the number of characters in "cześć" word is: 5
+    println!(
+        "the number of characters in \"{greating}\" word is: {}",
+        greating.chars().count()
+    );
+}
